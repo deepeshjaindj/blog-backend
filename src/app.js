4308b18aeadd218ee7ec6fc40776053fd4e3890d
deepeshjaindj <jainsahabdj07@gmail.com>
2024-06-09 00:32:01 +0530

    Add CORS support and configured necessary middlewares

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 import express from 'express';
+import cors from 'cors';
+import { CORS_ORIGIN } from './constants.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(cors({
+  origin: CORS_ORIGIN,
+  credentials: true
+}));
 
+app.use(express.json({
+  limit: '16kb'
+}));
+
+app.use(express.urlencoded({
+  extended: true,
+  limit: '16kb'
+}));
+
+app.use(express.static('public'));
+
+app.use(cookieParser());
 
 export { app };
--- src/constants.js
+++ src/constants.js
@@ -7,3 +7,5 @@ export const PORT = process.env.PORT;
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const DB_NAME = "Blog";
 
+// CORS
+export const CORS_ORIGIN = process.env.CORS_ORIGIN;

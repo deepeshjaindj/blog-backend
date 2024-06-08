31df5be382aa780c482ea883880a9af924181323
deepeshjaindj <jainsahabdj07@gmail.com>
2024-06-09 01:06:14 +0530

    Add cloudinary dependency and uploadOnCloudinary utility

--- src/constants.js
+++ src/constants.js
@@ -9,3 +9,8 @@ export const DB_NAME = "Blog";
 
 // CORS
 export const CORS_ORIGIN = process.env.CORS_ORIGIN;
+
+// CLOUDINARY
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
---
+++ src/utils/uploadOnCloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+import { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME } from '../constants';
+
+cloudinary.config({ 
+  cloud_name: CLOUDINARY_CLOUD_NAME, 
+  api_key: CLOUDINARY_API_KEY, 
+  api_secret: CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: 'auto'
+    });
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };

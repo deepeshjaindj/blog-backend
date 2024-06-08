023b95d63e4cf31c462aca1206e0f63f4cedd393
deepeshjaindj <jainsahabdj07@gmail.com>
2024-06-09 00:48:32 +0530

    Add error handling to asyncHandler.js

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,14 @@
+import { ApiError } from "./ApiError";
+
 const asyncHandler = (requestHandler) => async (req, res, next) => {
   try {
     await requestHandler(req, res, next);
   } catch(err) {
-    res.status(err.statusCode || 500);
+    res
+      .status(err.statusCode || 500)
+      .json(
+        new ApiError(err.statusCode, err.message, err.errors)
+      );
   }
 };
 

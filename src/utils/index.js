3387f391ed02f4deac6d4ffff695a7de11a1ae5b
deepeshjaindj <jainsahabdj07@gmail.com>
2024-06-09 01:11:21 +0530

    Add multer middleware

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 export { ApiError } from "./ApiError.js";
 export { ApiResponse } from "./ApiResponse.js";
 export { asyncHandler } from "./asyncHandler.js";
+export { uploadOnCloudinary } from  "./uploadOnCloudinary.js";

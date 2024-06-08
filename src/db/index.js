7e619fa74ae13af43bb216277ad58ef43462cef6
deepeshjaindj <jainsahabdj07@gmail.com>
2024-06-09 00:25:45 +0530

    Add MongoDB connection success message

--- src/db/index.js
+++ src/db/index.js
@@ -4,9 +4,10 @@ import { DB_NAME, MONGODB_URI } from "../constants.js";
 const connectDB = async() => {
   try {
     await mongoose.connect(`${MONGODB_URI}/${DB_NAME}`);
+    console.log("MongoDB Connection Successful");
   } catch(error) {
     console.log("MongoDB Connection Failed! ", error);
-    
+
     // eslint-disable-next-line no-undef
     process.exit(1);
   }
